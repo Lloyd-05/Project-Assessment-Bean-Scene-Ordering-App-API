47ba5333f566a48ad66196b988c582a01a6805e8
Lloyd-05 <lloyd.ryu@studytafensw.edu.au>
2026-07-09 21:05:41 +1000

    fix: add populate method to menu item route

--- API/routes/menuRoutes.js
+++ API/routes/menuRoutes.js
@@ -30,9 +30,9 @@ const authorizeRole = require("../middleware/roleAuth");
  */
 router.get("/", auth, authorizeRole("staff", "manager"), async (req, res) => {
   try {
-        const menuItems = await Menu.find();
+        // const menuItems = await Menu.find();
 
-    // const menuItems = await Menu.find().populate("category", "name");
+    const menuItems = await Menu.find().populate("category", "name");
     res.json(menuItems);
   } catch (err) {
     res.status(500).json({ message: `Error fetching menu items: ${err.message}` });
