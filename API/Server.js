0a858dfa5f7ec0d9ba8170d2e56b7f473597c656
Lloyd-05 <lloyd.ryu@studytafensw.edu.au>
2026-07-11 23:00:00 +1000

    fix: change URL to become more align with the Android emulator

--- API/Server.js
+++ API/Server.js
@@ -51,7 +51,9 @@ app.get("/api/health", (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)});
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
   console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 
 
